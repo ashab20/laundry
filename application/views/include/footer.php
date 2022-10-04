528cacadf11b308a7473efd054abe608f09f6b16
ashab20 <ashabuddin20@hotmail.com>
2022-10-04 10:47:58 +0600

    frist commit

---
+++ application/views/include/footer.php
@@ -0,0 +1,26 @@
+<!-- ===============================================-->
+<!--    JavaScripts-->
+<!-- ===============================================-->
+<script src="<?= base_url('assets/js/jquery.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/popper.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/bootstrap.min.js') ?>"></script>
+<script src="<?= base_url('assets/lib/@fortawesome/all.min.js') ?>"></script>
+<script src="<?= base_url('assets/lib/stickyfilljs/stickyfill.min.js') ?>"></script>
+<script src="<?= base_url('assets/lib/sticky-kit/sticky-kit.min.js') ?>"></script>
+<script src="<?= base_url('assets/lib/is_js/is.min.js') ?>"></script>
+<script src="<?= base_url('assets/lib/lodash/lodash.min.js') ?>"></script>
+<script src="<?= base_url('assets/lib/perfect-scrollbar/perfect-scrollbar.js') ?>"></script>
+<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,600,700%7cPoppins:100,200,300,400,500,600,700,800,900&amp;display=swap" rel="stylesheet">
+<script src="<?= base_url('assets/lib/chart.js/Chart.min.js') ?>"></script>
+<script src="<?= base_url('assets/lib/datatables/js/jquery.dataTables.min.js') ?>"></script>
+<script src="<?= base_url('assets/lib/datatables-bs4/dataTables.bootstrap4.min.js') ?>"></script>
+<script src="<?= base_url('assets/lib/datatables.net-responsive/dataTables.responsive.js') ?>"></script>
+<script src="<?= base_url('assets/lib/datatables.net-responsive-bs4/responsive.bootstrap4.js') ?>"></script>
+<script src="<?= base_url('assets/lib/leaflet/leaflet.js') ?>"></script>
+<script src="<?= base_url('assets/lib/leaflet.markercluster/leaflet.markercluster.js') ?>"></script>
+<script src="<?= base_url('assets/lib/leaflet.tilelayer.colorfilter/leaflet-tilelayer-colorfilter.min.js') ?>"></script>
+<script src="<?= base_url('assets/js/theme.js') ?>"></script>
+
+</body>
+
+</html>
---
+++ application/views/include/header.php
@@ -0,0 +1,47 @@
+<?php
+defined('BASEPATH') or exit('No direct script access allowed');
+?>
+<!DOCTYPE html>
+<html lang="en-US" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+
+    <!-- ===============================================-->
+    <!--    Document Title-->
+    <!-- ===============================================-->
+    <title>Falcon | Dashboard &amp; WebApp Template</title>
+
+
+    <!-- ===============================================-->
+    <!--    Favicons-->
+    <!-- ===============================================-->
+    <link rel="apple-touch-icon" sizes="180x180" href="<?= base_url('assets/img/favicons/apple-touch-icon.png') ?>')?>">
+    <link rel="icon" type="image/png" sizes="32x32" href="<?= base_url('assets/img/favicons/favicon-32x32.png') ?>">
+    <link rel="icon" type="image/png" sizes="16x16" href="<?= base_url('assets/img/favicons/favicon-16x16.png') ?>">
+    <link rel="shortcut icon" type="image/x-icon" href="<?= base_url('assets/img/favicons/favicon.ico') ?>">
+    <link rel="manifest" href="<?= base_url('assets/img/favicons/manifest.json') ?>">
+    <meta name="msapplication-TileImage" content="<?= base_url('assets/img/favicons/mstile-150x150.png') ?>">
+    <meta name="theme-color" content="#ffffff">
+
+
+    <!-- ===============================================-->
+    <!--    Stylesheets-->
+    <!-- ===============================================-->
+    <script src="<?= base_url('assets/js/config.navbar-vertical.js') ?>"></script>
+    <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+    <link href="<?= base_url('assets/lib/perfect-scrollbar/perfect-scrollbar.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/lib/datatables-bs4/dataTables.bootstrap4.min.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/lib/datatables.net-responsive-bs4/responsive.bootstrap4.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/lib/leaflet/leaflet.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/lib/leaflet.markercluster/MarkerCluster.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/lib/leaflet.markercluster/MarkerCluster.Default.css') ?>" rel="stylesheet">
+    <link href="<?= base_url('assets/css/theme.css') ?>" rel="stylesheet">
+
+</head>
+
+
+<body>
